b44e303c3665a0930c4fc51deadb997dd2841915
itarck <itarck@gmail.com>
2021-12-16 16:53:19 +0800

    Send messages to all channels

--- catchat-full/src/backend/main/catchat_server/handler/api.clj
+++ catchat-full/src/backend/main/catchat_server/handler/api.clj
@@ -9,22 +9,27 @@
 
 (defmethod ig/init-key :catchat-server.handler.api/session
   [_ {:keys [random-message-sender]}]
-  (fn [request]
-    (httpkit/with-channel request channel
-      (println "web socket:" (str channel))
-                          
-      #_(let [{:keys [message-chan]} random-message-sender]
-        (go-loop []
-          (let [message (<! message-chan)]
-            (httpkit/send! channel (str message)))
-          (recur)))
-                          
-      (httpkit/on-receive channel (fn [data]
-                                    (let [new-message (m.chat-rooms/insert-new-message (read-string data))]
-                                      (println "receive new message:" (str new-message))
-                                      (httpkit/send! channel (str new-message)))))
-      (httpkit/on-close channel (fn [status]
-                                  (println channel "closed, status" status))))))
+  (let [session-ref (atom {})]
+    (fn [request]
+      (httpkit/with-channel request channel
+        (println "web socket:" (str channel))
+        (swap! session-ref assoc channel (java.util.Date.))
+                            
+        (let [{:keys [message-chan]} random-message-sender]
+          (go-loop []
+            (let [message (<! message-chan)]
+              (doseq [[ch _] @session-ref]
+                (httpkit/send! ch (str message))))
+            (recur)))
+
+        (httpkit/on-receive channel (fn [data]
+                                      (let [new-message (m.chat-rooms/insert-new-message (read-string data))]
+                                        (println "receive new message:" (str new-message))
+                                        (doseq [[ch _] @session-ref]
+                                          (httpkit/send! ch (str new-message))))))
+        (httpkit/on-close channel (fn [status]
+                                    (swap! session-ref dissoc channel)
+                                    (println channel "closed, status" status)))))))
 
 
 (defmethod ig/init-key :catchat-server.handler/api
@@ -36,6 +41,5 @@
       (str (m.chat-rooms/whoami)))
     (GET "/session" []
       session-handler)))
-
 
 
